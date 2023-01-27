b97dbb0aa1b264b2e2b2f4ffd6152403f48c7600
Ana Senne <anaclarasenne@gmail.com>
2023-01-27 11:46:58 -0300

    Subindo arquivos do projeto

---
+++ src/main/java/com/appfinancas/AppFinancasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFinancasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFinancasServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appfinancas/AppFinancasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFinancasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
